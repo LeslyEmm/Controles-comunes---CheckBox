888a9ccb4eb1ef4e8e873bf4aeb722bbe71a299b
LeslyEmm <lemm020626@gmail.com>
2023-04-03 14:40:28 -0500

    Agregar archivos de proyecto.

---
+++ WinFormsAppLesly/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Text = ""
+        If CheckBox1.Checked = True Then
+            Text = Text + "(Colorantes)"
+        End If
+        If CheckBox2.Checked = True Then
+            Text = Text + "(Estampado)"
+        End If
+        If CheckBox3.Checked = True Then
+            Text = Text + "(Acabado)"
+        End If
+    End Sub
+
+
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+        If CheckBox4.Checked = True Then
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+End Class
